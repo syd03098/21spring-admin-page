9769d43d5a32a60a654d6923a83d6882ab086e76
Junyeong Yim <xjuny1996@gmail.com>
2021-02-12 23:18:00 +0900

    Backend: 영화 제목/정보 API 추가
    Detail
    ======
     - 영화 모델 추가
       - PK: id (autofield)
       - 최대 제목 255자, 설명 2000자
     - 영화 API 추가
       - ~/api/movie
       - GET, POST, PUT, PATCH, DELETE
       - PUT, PATCH, DELETE는 id 필요

---
+++ server/api/model/movie/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.title
