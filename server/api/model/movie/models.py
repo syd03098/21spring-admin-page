3960ff2c3d8aef85926eff28a61b81c5efdbd9b9
nalgaeg1t <49203563+nalgaeg1t@users.noreply.github.com>
2021-02-13 23:13:27 +0900

    Backend: 영화 모델 수정
    Detail
    ======
    - 카테고리 추가
    - 카테고리 최대 255자

--- server/api/model/movie/models.py
+++ server/api/model/movie/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
     description = models.CharField(max_length=2000)
 
     def __str__(self):
