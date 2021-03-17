7e07f82fba8156c01982380a9f59541f1d1d225c
yonghoKim <syd03097@gmail.com>
2021-03-18 00:38:17 +0900

    feature/front-002-1 react-hot-loader 삭제

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { hot } from 'react-hot-loader/root';
 import App from './App';
 
-const Hot = hot(App);
-
-render(<Hot />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
