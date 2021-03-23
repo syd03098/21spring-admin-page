e55822094e9c4feb0b33f494e051ac04deedd37b
yonghoKim <syd03097@gmail.com>
2021-03-23 21:45:08 +0900

    feature/front-003 styled component 최적화

--- frontend/src/components/global/GlobalStyles.tsx
+++ frontend/src/components/global/GlobalStyles.tsx
@@ -1,28 +1,31 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
+const globalStyle = css`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    *:focus {
+        outline: none;
+    }
+
+    button {
+        outline: none;
+    }
+
+    body,
+    body * {
+        font-family: 'Helvetica Neue', -apple-system, sans-serif;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        text-rendering: optimizeLegibility;
+    }
+`;
+
 export default createGlobalStyle`
-  
   ${normalize}
-  
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  
-  *:focus {
-    outline: none;
-  }
-  
-  button {
-    outline: none;
-  }
-  
-  body, body * {
-    font-family: "Helvetica Neue",-apple-system, sans-serif;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    text-rendering: optimizeLegibility;
-  }
+  ${globalStyle}
 `;
