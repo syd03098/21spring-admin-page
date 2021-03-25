a69358074932056d82b5e254d1daa9d4cc2aee06
Junyeong Yim <xjuny1996@gmail.com>
2021-03-26 04:06:30 +0900

    Backend: 로그인 테스트 API 추가
    Detail
    ======
     - 아이디 최대 16바이트, 비밀번호 최대 20바이트, 닉네임 최대 30바이트
     - 아이디와 비밀번호는 빈 값이 올 수 없음
     - 아이디는 영문, 숫자, 언더바, 점 으로만 생성 가능
     - /account
       - GET: 회원 목록
       - POST: 회원 가입
         - 정상 생성: 201
         - 바이트 수 제한 초과: 400
         - 사용할 수 없는 아이디: 400
         - 중복된 아이디 존재: 409
     - /signin
       - POST: 로그인
         - 로그인 성공: 200 (응답: userid, name으로 된 json)
         - 바이트 수 제한 초과: 400
         - 비밀번호 틀림: 401
         - 존재하지 않는 아이디: 404

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,6 +1,12 @@
+import hashlib
+import re
+
 from rest_framework import serializers
+from rest_framework.exceptions import APIException
+from rest_framework.serializers import ValidationError
 
 from api.model.movie.models import Movie
+from api.model.account_test.models import Account
 
 
 class MovieSerializer(serializers.ModelSerializer):
@@ -8,3 +14,45 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    userid = serializers.CharField(max_length=16)
+    passwd = serializers.CharField(max_length=20)
+    name = serializers.CharField(max_length=30)
+
+    def validate_userid(self, value):
+        if re.match('.*[^A-Za-z0-9_.].*', value):
+            raise ValidationError('아이디는 영문, 숫자, 언더바, 점만 올 수 있습니다.')
+        return value
+
+    def create(self, validated_data):
+        userid = validated_data['userid']
+        passwd = hashlib.sha256(validated_data['passwd'].encode()).hexdigest()
+        if Account.objects.filter(userid=userid).exists():
+
+            class ConflictException(APIException):
+                status_code = 409
+                default_detail = '중복된 아이디가 존재합니다.'
+                default_code = '409 Conflict'
+
+            raise ConflictException()
+        return Account.objects.create(userid=userid,
+                                      passwd=passwd,
+                                      name=validated_data['name'])
+
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+
+class SignInSerializer(serializers.ModelSerializer):
+    userid = serializers.CharField(max_length=16)
+    passwd = serializers.CharField(max_length=20)
+
+    class Meta:
+        model = Account
+        fields = (
+            'userid',
+            'passwd',
+        )
