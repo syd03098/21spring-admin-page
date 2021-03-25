386c8fbb874ef52419b496935067d71615a263c3
Junyeong Yim <xjuny1996@gmail.com>
2021-03-26 04:06:30 +0900

    Backend: 로그인 테스트 API 추가
    Detail
    ======
     - 아이디 최대 16바이트, 비밀번호 최대 20바이트, 닉네임 최대 30바이트
     - 아이디와 비밀번호는 빈 값이 올 수 없음
     - 아이디는 영문, 숫자, 언더바, 점 으로만 생성 가능
     - /account
       - GET: 회원 목록
       - POST: 회원 가입
         - 정상 생성: 201
         - 바이트 수 제한 초과: 400
         - 사용할 수 없는 아이디: 400
         - 중복된 아이디 존재: 409
     - /signin
       - POST: 로그인
         - 로그인 성공: 200 (응답: userid, name으로 된 json)
         - 바이트 수 제한 초과: 400
         - 비밀번호 틀림: 401
         - 존재하지 않는 아이디: 404

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
-from django.urls import path
 from rest_framework import routers
 
-from api.views import MovieViewSet
+from api.views import MovieViewSet, AccountViewSet, SignInViewSet
 
 router = routers.DefaultRouter()
 router.register(r'movies', MovieViewSet, basename='movies')
+router.register(r'account', AccountViewSet, basename='account')
+router.register(r'signin', SignInViewSet, basename='signin')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- server/api/views.py
+++ server/api/views.py
@@ -1,11 +1,43 @@
+import hashlib
+
+from django.http.response import JsonResponse
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from api.model.movie.models import Movie
+from api.model.account_test.models import Account
 
-from .serializers import MovieSerializer
+from .serializers import MovieSerializer, AccountSerializer, SignInSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     lookup_url_kwarg = 'id'
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    http_method_names = ['post', 'get']
+
+
+class SignInViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = SignInSerializer
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        userid = request.data.get('userid')
+        passwd = hashlib.sha256(request.data.get('passwd').encode()).hexdigest()
+        name = request.data.get('name')
+
+        account = Account.objects.filter(userid=userid).first()
+        if not account:
+            return Response(status=404, data='존재하지 않는 아이디 입니다.')
+        if account.passwd != passwd:
+            return Response(status=401, data='비밀번호가 틀렸습니다.')
+
+        return JsonResponse({'userid': userid, 'name': name}, status=200)
