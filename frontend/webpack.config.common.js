6d680230523e86ac848e49e8fff389872fd9238d
yonghoKim <syd03097@gmail.com>
2021-04-18 01:17:25 +0900

    feature/front/initial-components directory 추가

--- frontend/webpack.config.common.js
+++ frontend/webpack.config.common.js
@@ -10,6 +10,7 @@ module.exports = {
             '@hooks': path.resolve(__dirname, './src/hooks'),
             '@stores': path.resolve(__dirname, './src/stores'),
             '@pages': path.resolve(__dirname, './src/pages'),
+            '@utils': path.resolve(__dirname, './src/utils'),
         },
     },
     module: {
