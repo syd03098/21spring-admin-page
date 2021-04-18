f44b6dc3371b7f7482c8056a803bfd805c9f158d
yonghoKim <syd03097@gmail.com>
2021-04-19 00:52:52 +0900

    feature/front/initial-components desktop, mobile 너비 수정

--- frontend/src/components/atom/ShowMobile.tsx
+++ frontend/src/components/atom/ShowMobile.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 const ShowMobile = ({ children }: { children: ReactNode }): JSX.Element => {
-    const isMobile = useMediaQuery({ query: '(max-width:799px)' });
+    const isMobile = useMediaQuery({ query: '(max-width:699px)' });
     return <>{isMobile && children}</>;
 };
 
