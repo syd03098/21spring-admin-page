10fc30bc658c86012a4198a5f0dc6b83807ca698
yonghoKim <syd03097@gmail.com>
2021-04-29 01:52:42 +0900

    feature/front/initial-components toast에서 사용할 애니메이션

---
+++ frontend/src/utils/animations.ts
@@ -0,0 +1,7 @@
+import { keyframes } from 'styled-components';
+
+export const bounce = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(1.1); }
+  100% { transform: scale(1); }
+`;
