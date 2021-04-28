b6cf5aa6cd5c3a8bc617f823d29d9828718766d1
yonghoKim <syd03097@gmail.com>
2021-04-29 02:44:00 +0900

    front/feature/toastList 쓸데없는 store 제거

--- frontend/src/pages/EntranceHallPage.tsx
+++ frontend/src/pages/EntranceHallPage.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import { observer } from 'mobx-react';
-import { useRootStore } from '@stores/RootStore';
 
-const EntranceHallPage = observer(
-    (): JSX.Element => {
-        const { testStore } = useRootStore();
-        const { testId, setTestId } = testStore;
-
-        return (
-            <>
-                <div>{testId}</div>
-                <button
-                    type="button"
-                    onClick={(): void => {
-                        setTestId();
-                    }}
-                >
-                    +1
-                </button>
-            </>
-        );
-    },
-);
+const EntranceHallPage = (): JSX.Element => {
+    return <></>;
+};
 
 export default EntranceHallPage;
