ab300db3f6a156e9d9261fe1889af3fcd4026f94
yonghoKim <syd03097@gmail.com>
2021-04-29 02:44:00 +0900

    front/feature/toastList 쓸데없는 store 제거

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,25 +1,20 @@
 import React, { useCallback } from 'react';
-import { observer } from 'mobx-react';
 import styled from 'styled-components';
 import LoginForm from '@components/molecule/LoginForm';
 
-const LoginPage = observer(
-    (): JSX.Element => {
-        const onSubmit = useCallback(() => {
-            console.log('!!!');
-        }, []);
+const LoginPage = (): JSX.Element => {
+    const onSubmit = useCallback(() => {}, []);
 
-        return (
-            <>
-                <StyledWrap>
-                    <StyledContainer>
-                        <LoginForm onSubmit={onSubmit} />
-                    </StyledContainer>
-                </StyledWrap>
-            </>
-        );
-    },
-);
+    return (
+        <>
+            <StyledWrap>
+                <StyledContainer>
+                    <LoginForm onSubmit={onSubmit} />
+                </StyledContainer>
+            </StyledWrap>
+        </>
+    );
+};
 
 const StyledWrap = styled.div`
     width: 100%;
