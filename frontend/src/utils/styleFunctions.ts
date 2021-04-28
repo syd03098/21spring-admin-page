6a5b991488d079926b204175d2b00192b13e5524
yonghoKim <syd03097@gmail.com>
2021-04-29 01:52:12 +0900

    feature/front/initial-components modal에서 사용할 preventScrollLock globalStyle

--- frontend/src/utils/styleFunctions.ts
+++ frontend/src/utils/styleFunctions.ts
@@ -1,5 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
 export const flexCenter = (): string => `
     display: flex;
     align-items: center;
     justify-content: center;
+`;
+
+export const StyledPreventScroll = createGlobalStyle`
+  body {
+    overflow: hidden;
+  }
 `;
