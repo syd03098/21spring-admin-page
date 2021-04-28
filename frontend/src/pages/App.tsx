5af9d933752530658e2879dc73a9f3177e608070
yonghoKim <syd03097@gmail.com>
2021-04-29 02:42:32 +0900

    feature/front/toastList toastList, ToastStore 추가

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,10 +1,11 @@
 import React, { Suspense, lazy } from 'react';
-import { RootStoreProvider } from '@stores/RootStore';
 import theme from '@utils/theme';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '@components/atom/GlobalStyles';
 import ErrorBoundary from '@components/molecule/ErrorBoundary';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ToastStoreProvider } from '@stores/ToastStore';
+import ToastList from '@components/molecule/toastList';
 
 const entrance = lazy(() => import('@pages/EntranceHallPage'));
 const login = lazy(() => import('@pages/LoginPage'));
@@ -16,12 +17,13 @@ const App = (): JSX.Element => {
             <Suspense fallback={<div />}>
                 <ErrorBoundary>
                     <Router>
-                        <RootStoreProvider>
+                        <ToastStoreProvider>
                             <Switch>
                                 <Route exact path="/" component={entrance} />
                                 <Route exact path="/login" component={login} />
                             </Switch>
-                        </RootStoreProvider>
+                            <ToastList />
+                        </ToastStoreProvider>
                     </Router>
                 </ErrorBoundary>
             </Suspense>
