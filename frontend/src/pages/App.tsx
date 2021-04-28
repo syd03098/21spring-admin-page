da312047f29e3aa1b7b73e57851aa31e52d4d416
yonghoKim <syd03097@gmail.com>
2021-04-29 01:50:10 +0900

    feature/front/initial-components 앞으로 작업할 페이지 나누기

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,13 +1,15 @@
 import React, { Suspense, lazy } from 'react';
-import { RootStoreProvider } from '@stores/RootStore';
 import theme from '@utils/theme';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from '@components/atom/GlobalStyles';
-import ErrorBoundary from '@components/molecule/ErrorBoundary';
+import GlobalStyles from '@pages/GlobalStyles';
+import ErrorBoundary from '@components/molecule/errorBoundary';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ToastStoreProvider } from '@stores/ToastStore';
+import ToastList from '@components/molecule/toastList';
 
-const entrance = lazy(() => import('@pages/EntranceHallPage'));
-const login = lazy(() => import('@pages/LoginPage'));
+const entrance = lazy(() => import('@pages/EntranceHall'));
+const login = lazy(() => import('@pages/login'));
+const profile = lazy(() => import('@pages/Profile'));
 
 const App = (): JSX.Element => {
     return (
@@ -16,12 +18,15 @@ const App = (): JSX.Element => {
             <Suspense fallback={<div />}>
                 <ErrorBoundary>
                     <Router>
-                        <RootStoreProvider>
+                        <ToastStoreProvider>
                             <Switch>
                                 <Route exact path="/" component={entrance} />
                                 <Route exact path="/login" component={login} />
+                                <Route exact path="/create" component={login} />
+                                <Route exact path="/profile" component={profile} />
                             </Switch>
-                        </RootStoreProvider>
+                            <ToastList />
+                        </ToastStoreProvider>
                     </Router>
                 </ErrorBoundary>
             </Suspense>
---
+++ frontend/src/pages/EntranceHall.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EntranceHall = (): JSX.Element => {
+    return <>entrance</>;
+};
+
+export default EntranceHall;
