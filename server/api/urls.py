a0e9a0d31b53abb2f9c49b36bdee7960957b579a
임준영 <limejuny@gmail.com>
2021-05-06 23:46:39 +0900

    Backend: 회원가입 API 추가
    Detail
    ======
     - 회원가입 API 추가
     - 쿠키 x
     - USR.USR_PASSWORD CHAR(64)로 변경
     - 비회원용 시퀀스 추가

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from api.views import MovieViewSet, AccountViewSet, SignInViewSet
+# from api.views import MovieViewSet, AccountViewSet, SignInViewSet
+from api.views import UsrViewSet
 
 router = routers.DefaultRouter()
-router.register(r'movies', MovieViewSet, basename='movies')
-router.register(r'account', AccountViewSet, basename='account')
-router.register(r'signin', SignInViewSet, basename='signin')
+router.register(r'auth/create', UsrViewSet, basename='usrcreate')
+# router.register(r'movies', MovieViewSet, basename='movies')
+# router.register(r'account', AccountViewSet, basename='account')
+# router.register(r'signin', SignInViewSet, basename='signin')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
