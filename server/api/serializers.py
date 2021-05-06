6a65f98dc07010c38d102fbc2415188acd410ba2
Junyeong Yim <xjuny1996@gmail.com>
2021-05-06 23:46:39 +0900

    Backend: 회원가입 API 추가
    Detail
    ======
     - 회원가입 API 추가
     - 쿠키 x
     - USR.USR_PASSWORD CHAR(64)로 변경
     - 비회원용 시퀀스 추가

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,58 +1,25 @@
-import hashlib
 import re
 
 from rest_framework import serializers
-from rest_framework.exceptions import APIException
 from rest_framework.serializers import ValidationError
 
-from api.model.movie.models import Movie
-from api.model.account_test.models import Account
+from api.model.models import Usr
 
 
-class MovieSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Movie
-        fields = '__all__'
-
-
-class AccountSerializer(serializers.ModelSerializer):
+class UsrCreateSerializer(serializers.Serializer):
     userid = serializers.CharField(max_length=16)
-    passwd = serializers.CharField(max_length=20)
-    name = serializers.CharField(max_length=30)
+    username = serializers.CharField(max_length=30)
+    email = serializers.CharField(max_length=50)
+    password = serializers.CharField(max_length=20)
 
     def validate_userid(self, value):
         if re.match('.*[^A-Za-z0-9_.].*', value):
             raise ValidationError('아이디는 영문, 숫자, 언더바, 점만 올 수 있습니다.')
         return value
 
-    def create(self, validated_data):
-        userid = validated_data['userid']
-        passwd = hashlib.sha256(validated_data['passwd'].encode()).hexdigest()
-        if Account.objects.filter(userid=userid).exists():
 
-            class ConflictException(APIException):
-                status_code = 409
-                default_detail = '중복된 아이디가 존재합니다.'
-                default_code = '409 Conflict'
-
-            raise ConflictException()
-        return Account.objects.create(userid=userid,
-                                      passwd=passwd,
-                                      name=validated_data['name'])
+class UsrSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Account
+        model = Usr
         fields = '__all__'
-
-
-class SignInSerializer(serializers.ModelSerializer):
-    userid = serializers.CharField(max_length=16)
-    passwd = serializers.CharField(max_length=20)
-
-    class Meta:
-        model = Account
-        fields = (
-            'userid',
-            'passwd',
-        )
