f8709c962578feecdd621d14389ad2da45663019
Junyeong Yim <xjuny1996@gmail.com>
2021-05-07 22:53:59 +0900

    Backend: Add auth/login API
    Detail
    ======
     - Add auth/login API

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -3,8 +3,6 @@ import re
 from rest_framework import serializers
 from rest_framework.serializers import ValidationError
 
-from api.model.models import Usr
-
 
 class UsrCreateSerializer(serializers.Serializer):
     userid = serializers.CharField(max_length=16)
@@ -18,8 +16,19 @@ class UsrCreateSerializer(serializers.Serializer):
         return value
 
 
-class UsrSerializer(serializers.ModelSerializer):
+class UsrLoginSerializer(serializers.Serializer):
+    userid = serializers.CharField(max_length=16)
+    password = serializers.CharField(max_length=20)
 
-    class Meta:
-        model = Usr
-        fields = '__all__'
+    def validate_userid(self, value):
+        if re.match('.*[^A-Za-z0-9_.].*', value):
+            raise ValidationError('아이디는 영문, 숫자, 언더바, 점만 올 수 있습니다.')
+        return value
+
+
+class UsrSerializer(serializers.Serializer):
+    userid = serializers.CharField(max_length=16)
+    username = serializers.CharField(max_length=30)
+    email = serializers.CharField(max_length=50)
+    point = serializers.IntegerField()
+    isAdmin = serializers.BooleanField()
