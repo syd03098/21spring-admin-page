e1c8f4790fe97e2a045bfab15ef1d68b2086884c
Junyeong Yim <xjuny1996@gmail.com>
2021-05-07 22:53:59 +0900

    Backend: Add auth/login API
    Detail
    ======
     - Add auth/login API

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import include, url
 from rest_framework import routers
 
 # from api.views import MovieViewSet, AccountViewSet, SignInViewSet
-from api.views import UsrViewSet
+from api.views import UsrViewSet, LoginViewSet
 
 router = routers.DefaultRouter()
 router.register(r'auth/create', UsrViewSet, basename='usrcreate')
+router.register(r'auth/login', LoginViewSet, basename='login')
 # router.register(r'movies', MovieViewSet, basename='movies')
 # router.register(r'account', AccountViewSet, basename='account')
 # router.register(r'signin', SignInViewSet, basename='signin')
