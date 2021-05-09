7819e632f25c61be2c8f2856d74e0121a0de4583
yonghoKim <syd03097@gmail.com>
2021-05-09 19:36:13 +0900

    Desktop, Mobile 기준 width 재조정

--- frontend/src/components/atom/Mobile.tsx
+++ frontend/src/components/atom/Mobile.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 import useMedia from 'react-use/lib/useMedia';
 
 const Mobile = ({ children }: { children: ReactNode }): JSX.Element => {
-    const isMobile = useMedia('(max-width:699px)');
+    const isMobile = useMedia('(max-width:719px)');
     return <>{isMobile && children}</>;
 };
 
