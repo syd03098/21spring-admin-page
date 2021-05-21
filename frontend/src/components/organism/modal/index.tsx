8f8fa1fbc39dfbed36e5ab5c56bd02dc5ed78db2
syd03098 <syd03097@gmail.com>
2021-05-22 01:29:12 +0900

    feature/front/select_movie_info 영화 상세정보 보기

--- frontend/src/components/organism/modal/index.tsx
+++ frontend/src/components/organism/modal/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import { useModal } from '@stores/ModalStore';
+import styled from 'styled-components';
+import { fullDisplay } from '@utils/styleFunctions';
 
-const Modal = (): JSX.Element => {
-    const { Modal: StyledModal, contents } = useModal();
-    return <StyledModal>{contents}</StyledModal>;
+const SharedModal = (): JSX.Element => {
+    const { Portal: StyledPortal, contents, overlayRef, isOpen } = useModal();
+    return (
+        <>
+            {isOpen && (
+                <StyledPortal>
+                    <StyledBackground>
+                        <StyledOverlay ref={overlayRef}>{contents}</StyledOverlay>
+                    </StyledBackground>
+                </StyledPortal>
+            )}
+        </>
+    );
 };
 
-export default Modal;
+const StyledBackground = styled.div`
+    position: fixed;
+    ${fullDisplay};
+    z-index: 1000;
+`;
+
+const StyledOverlay = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(0, 0, 0, 0.4);
+`;
+
+export default SharedModal;
