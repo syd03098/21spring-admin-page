b6dfb6cf13b7fabe2767fd545b722a4c499110bd
Junyeong Yim <xjuny1996@gmail.com>
2021-05-29 03:03:20 +0900

    Backend: 포인트로 예매 기능 추가
    Detail
    ======
     - 포인트로 예매 기능 추가
     - 카드로 예매시 결제금액의 10% 적립 기능 추가
     - 레거시 모델 제거 (account_test, moviemovie)

--- server/core/pay.py
+++ server/core/pay.py
@@ -2,8 +2,11 @@ import datetime
 
 from django.db import connection
 
+from api.model.models import (
+    Usr,)
 
-def pay_card(request, money, userId) -> int:
+
+def pay_card(money, userId) -> int:
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with connection.cursor() as cursor:
         cursor.execute("INSERT INTO PAY VALUES (PAY_SEQ.NEXTVAL, 1, 2, " \
@@ -11,22 +14,33 @@ def pay_card(request, money, userId) -> int:
                 f"TO_DATE('{now}', 'YYYY-MM-DD HH24:MI:SS') );")
         cursor.execute("SELECT PAY_SEQ.CURRVAL FROM DUAL;")
         pay_id = cursor.fetchone()[0]
+        cursor.execute(f"SELECT USR_POINT FROM USR WHERE USR_ID='{userId}';")
+        point = cursor.fetchone()[0] + money // 10
+        cursor.execute(
+            f"UPDATE USR SET USR_POINT={point} " \
+                    f"WHERE USR_ID='{userId}';")
     return pay_id
 
 
-def pay_point(request, money, userId) -> int:
+def pay_point(money, userId) -> int:
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    user = Usr.objects.raw(f"SELECT * FROM USR WHERE USR_ID='{userId}'")[0]
+    point = user.usr_point
+    if point < money:
+        raise Exception(f"포인트가 부족합니다. 잔여 포인트: {point}, 금액: {money}")
     with connection.cursor() as cursor:
         cursor.execute("INSERT INTO PAY VALUES (PAY_SEQ.NEXTVAL, 2, 2, " \
                 f"{money}, APRV_SEQ.NEXTVAL, " \
                 f"TO_DATE('{now}', 'YYYY-MM-DD HH24:MI:SS') );")
         cursor.execute("SELECT PAY_SEQ.CURRVAL FROM DUAL;")
         pay_id = cursor.fetchone()[0]
-    # TODO: 포인트 차감, 포인트 부족시 예외 발생
+        cursor.execute(
+            f"UPDATE USR SET USR_POINT={point-money} WHERE USR_ID='{userId}';")
+
     return pay_id
 
 
-def pay_cash(request, money, userId) -> int:
+def pay_cash(money, userId) -> int:
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with connection.cursor() as cursor:
         cursor.execute("INSERT INTO PAY VALUES (PAY_SEQ.NEXTVAL, 3, 1, " \
@@ -37,7 +51,7 @@ def pay_cash(request, money, userId) -> int:
     return pay_id
 
 
-def pay(request, pay_type: int, money: int, userId: str) -> int:
+def pay(pay_type: int, money: int, userId: str) -> int:
     _pay = {
         1: pay_card,
         2: pay_point,
@@ -46,4 +60,4 @@ def pay(request, pay_type: int, money: int, userId: str) -> int:
     pay = _pay.get(pay_type)
     if not pay:
         raise Exception(f"payType: {pay_type}은 올바르지 않은 결제 유형입니다.")
-    return pay(request, money, userId)
+    return pay(money, userId)
