6fab6ecb73ee395bd1be915518fb2a6eaf6b5799
syd03098 <syd03097@gmail.com>
2021-05-30 03:11:23 +0900

    feature/hotfix/validation fix infinite Loading

--- frontend/src/pages/entrance/index.tsx
+++ frontend/src/pages/entrance/index.tsx
@@ -1,29 +1,14 @@
 import React from 'react';
 import GlobalNavbar from '@components/organism/globalNavbar';
 import HorizontalSwiper from '@components/organism/horizontalSwiper';
-import styled from 'styled-components';
-import Loading from '@pages/Loading';
 
 const Entrance = (): JSX.Element => {
     return (
-        <Loading>
-            <Layout>
-                <GlobalNavbar />
-                <HorizontalSwiper />
-            </Layout>
-        </Loading>
+        <>
+            <GlobalNavbar />
+            <HorizontalSwiper />
+        </>
     );
 };
-
-const Layout = styled.div`
-    @media (min-width: 720px) {
-        padding-top: 80px;
-        padding-bottom: 60px;
-    }
-    @media (max-width: 719px) {
-        padding-top: 64px;
-        padding-bottom: 48px;
-    }
-`;
 
 export default Entrance;
